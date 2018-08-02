4f243e25a0d5aacff2115059d6824217ddca15c7
khangtn <tnkhang2405@gmail.com>
2018-08-02 20:51:06 +0700

    update config

--- config.py
+++ config.py
@@ -3,10 +3,15 @@
 # host = "ds237669.mlab.com"
 # port = 37669
 
-server_ip = 'localhost'
+server_ip = '0.0.0.0'
 server_port =  3333
-host = "localhost"
-port = 27017
+host = "ds155278.mlab.com"
+port = 55278
+
+# server_ip = 'localhost'
+# server_port =  3333
+# host = "localhost"
+# port = 27017
 
 username = "root"
 password = "123"
